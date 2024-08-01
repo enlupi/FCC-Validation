5c6b900dba80ae298804eddbb09ac40e0ef120ab
Enrico Lupi <enrico.lupi@cern.ch>
2024-08-01 16:46:36 +0200

    ADD setup in plot phase

--- Val_Scripts/ARC_standalone_o1_v01/ARC_standalone_o1_v01_plot.sh
+++ Val_Scripts/ARC_standalone_o1_v01/ARC_standalone_o1_v01_plot.sh
@@ -1,4 +1,6 @@
 # make plots
+source /cvmfs/sw.hsf.org/key4hep/setup.sh
+
 python FCC_Validation/Analysis/ARC_val_plots.py -f ARC_sim.root --no_show \
        -o key4hep-reco-validation/www/ARC_standalone_o1_v01/
 
