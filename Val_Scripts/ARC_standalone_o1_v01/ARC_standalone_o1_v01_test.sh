4913bda3561b9ac731014d43780269cb065ee2cc
Enrico Lupi <enrico.lupi@cern.ch>
2024-08-01 15:05:13 +0200

    ADD make_web part

--- Val_Scripts/ARC_standalone_o1_v01/ARC_standalone_o1_v01_test.sh
+++ Val_Scripts/ARC_standalone_o1_v01/ARC_standalone_o1_v01_test.sh
@@ -9,10 +9,13 @@ mkdir -p key4hep-reco-validation/www/ARC_standalone_o1_v01/
 # simulation phase
 ddsim --steeringFile FCC_Validation/Steering_Files/ARC_steering.py \
       --compactFile k4geo/test/compact/ARC_standalone_o1_v01.xml \
-      -N 10000
+      -N 100
 
 # make plots
 python FCC_Validation/Analysis/ARC_val_plots.py -f ARC_sim.root --no_show \
        -o key4hep-reco-validation/www/ARC_standalone_o1_v01/
 
 
+# upload them on website
+cd key4hep-reco-validation/web/python
+python3 make_web.py --dest ../../www
