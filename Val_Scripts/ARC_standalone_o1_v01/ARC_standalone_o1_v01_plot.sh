3d61135dc5589ef9fc3293a333fe40707ae8ca05
Enrico Lupi <enrico.lupi@cern.ch>
2024-08-02 16:15:33 +0200

    ADD verbosity

--- Val_Scripts/ARC_standalone_o1_v01/ARC_standalone_o1_v01_plot.sh
+++ Val_Scripts/ARC_standalone_o1_v01/ARC_standalone_o1_v01_plot.sh
@@ -1,10 +1,15 @@
 # make plots
+echo "Sourcing key4hep..."
 source /cvmfs/sw.hsf.org/key4hep/setup.sh
+echo "Sourcing executed successfully"
 
+echo "Starting analysis and plot script..."
 python FCC_Validation/Analysis/ARC_val_plots.py -f ARC_sim.root --no_show \
        -o key4hep-reco-validation/www/ARC_standalone_o1_v01/validation
-
+echo "Script executed successfully"
 
 # upload them on website
+echo "Starting website script..."
 cd key4hep-reco-validation/web/python
 python3 make_web.py --dest ../../www
+echo "Script executed successfully"
--- Val_Scripts/ARC_standalone_o1_v01/ARC_standalone_o1_v01_simulation.sh
+++ Val_Scripts/ARC_standalone_o1_v01/ARC_standalone_o1_v01_simulation.sh
@@ -1,6 +1,10 @@
 # simulation phase
+echo "Sourcing key4hep..."
 source /cvmfs/sw.hsf.org/key4hep/setup.sh
+echo "Sourcing executed successfully"
 
+echo "Starting simulation..."
 ddsim --steeringFile FCC_Validation/Steering_Files/ARC_steering.py \
       --compactFile k4geo/test/compact/ARC_standalone_o1_v01.xml \
       -N 100
+echo "Simulation ended successfully"
