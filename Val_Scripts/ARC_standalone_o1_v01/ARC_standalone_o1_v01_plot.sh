b7481b7bc0062bdb530c34cff9bf93b3cebee059
Enrico Lupi <enrico.lupi@cern.ch>
2024-08-05 13:39:56 +0200

    ADD PLOTAREA directory for website

--- Val_Scripts/ARC_standalone_o1_v01/ARC_standalone_o1_v01_plot.sh
+++ Val_Scripts/ARC_standalone_o1_v01/ARC_standalone_o1_v01_plot.sh
@@ -5,11 +5,11 @@ echo "Sourcing executed successfully"
 
 echo "Starting analysis and plot script..."
 python FCC_Validation/Analysis/ARC_val_plots.py -f ARC_sim.root --no_show \
-       -o key4hep-reco-validation/www/ARC_standalone_o1_v01/validation/plots
+       -o $PLOTAREA/ARC_standalone_o1_v01/validation/plots
 echo "Script executed successfully"
 
 # upload them on website
 echo "Starting website script..."
 cd key4hep-reco-validation/web/python
-python3 make_web.py --dest ../../www
+python3 make_web.py --dest $PLOTAREA
 echo "Script executed successfully"
--- Val_Scripts/ARC_standalone_o1_v01/ARC_standalone_o1_v01_setup.sh
+++ Val_Scripts/ARC_standalone_o1_v01/ARC_standalone_o1_v01_setup.sh
@@ -3,8 +3,7 @@
 echo "Downloading necessary Github repos..."
 
 git clone --depth 1 https://github.com/key4hep/k4geo.git
-# git clone https://github.com/key4hep/key4hep-reco-validation.git
-git clone https://github.com/enlupi/key4hep-reco-validation.git
-mkdir -p key4hep-reco-validation/www/ARC_standalone_o1_v01/validation/plots
+#git clone https://github.com/key4hep/key4hep-reco-validation.git
+mkdir -p $PLOTAREA/ARC_standalone_o1_v01/validation/plots
 
 echo "Download terminated - setup stage successful"
