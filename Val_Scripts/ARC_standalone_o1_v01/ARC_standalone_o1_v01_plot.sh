4e014efbe7f3d3cbbd250da3fdb996bb9c691c4e
Enrico Lupi <enrico.lupi@cern.ch>
2024-08-05 11:25:32 +0200

    FIX output plot directory

--- Val_Scripts/ARC_standalone_o1_v01/ARC_standalone_o1_v01_plot.sh
+++ Val_Scripts/ARC_standalone_o1_v01/ARC_standalone_o1_v01_plot.sh
@@ -5,7 +5,7 @@ echo "Sourcing executed successfully"
 
 echo "Starting analysis and plot script..."
 python FCC_Validation/Analysis/ARC_val_plots.py -f ARC_sim.root --no_show \
-       -o key4hep-reco-validation/www/ARC_standalone_o1_v01/validation
+       -o key4hep-reco-validation/www/ARC_standalone_o1_v01/validation/plots
 echo "Script executed successfully"
 
 # upload them on website
