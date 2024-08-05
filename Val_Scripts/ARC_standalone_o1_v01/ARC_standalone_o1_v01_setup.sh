943e22acf6868461d8037bb2a19ce9a65521b2ab
Enrico Lupi <enrico.lupi@cern.ch>
2024-08-05 13:49:03 +0200

    FIX PLOTAREA directory for website

--- Val_Scripts/ARC_standalone_o1_v01/ARC_standalone_o1_v01_setup.sh
+++ Val_Scripts/ARC_standalone_o1_v01/ARC_standalone_o1_v01_setup.sh
@@ -4,6 +4,6 @@ echo "Downloading necessary Github repos..."
 
 git clone --depth 1 https://github.com/key4hep/k4geo.git
 #git clone https://github.com/key4hep/key4hep-reco-validation.git
-mkdir -p $PLOTAREA/ARC_standalone_o1_v01/validation/plots
+mkdir -p $WORKAREA/$PLOTAREA/ARC_standalone_o1_v01/validation/plots
 
 echo "Download terminated - setup stage successful"
