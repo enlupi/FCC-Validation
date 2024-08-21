947a1cafa639d87c8a38cfc0499b23b292428370
Enrico Lupi <enrico.lupi@cern.ch>
2024-08-21 11:51:37 +0200

    Remove scripts for ARC_standalone (now updated in FCCee-scripts)

--- Val_Scripts/ARC_standalone_o1_v01/ARC_standalone_o1_v01_setup.sh
+++
@@ -1,9 +0,0 @@
-# setup phase
-
-echo "Downloading necessary Github repos..."
-
-git clone --depth 1 https://github.com/key4hep/k4geo.git
-#git clone https://github.com/key4hep/key4hep-reco-validation.git
-mkdir -p $WORKAREA/$PLOTAREA/ARC_standalone_o1_v01/validation/plots
-
-echo "Download terminated - setup stage successful"
